2e28b5af835542d244d1cbc4dcf4883011187dbe
Jonas Irgens Kylling <jonas.kylling@cognite.com>
2020-03-11 17:41:09 +0100

    Don't mess up indentation.

--- grpc/src/main/java/com/lightstep/tracer/shared/GrpcCollectorClientProvider.java
+++ grpc/src/main/java/com/lightstep/tracer/shared/GrpcCollectorClientProvider.java
@@ -6,53 +6,53 @@ import io.grpc.ManagedChannelProvider;
 
 // public for reflective instantiation.
 public class GrpcCollectorClientProvider extends CollectorClientProvider {
-  private static GrpcCollectorClientProvider INSTANCE = new GrpcCollectorClientProvider();
+    private static GrpcCollectorClientProvider INSTANCE = new GrpcCollectorClientProvider();
 
-  public static GrpcCollectorClientProvider provider() {
-    return INSTANCE;
-  }
-
-  // public for reflective instantiation.
-  public GrpcCollectorClientProvider() {
-    ManagedChannelProvider.provider();
-  }
-
-  @Override
-  protected int priority() {
-    return 1;
-  }
-
-  @Override
-  GrpcCollectorClient forOptions(
-      AbstractTracer tracer,
-      Options options
-  ) {
-    try {
-      ManagedChannelBuilder<?> builder;
-      if (options.grpcCollectorTarget != null) {
-        builder = ManagedChannelBuilder.forTarget(options.grpcCollectorTarget);
-      } else {
-        builder = ManagedChannelBuilder.forAddress(
-            options.collectorUrl.getHost(),
-            options.collectorUrl.getPort()
-        );
-      }
-
-      if (options.grpcRoundRobin) {
-        builder.defaultLoadBalancingPolicy("round_robin");
-      }
-
-      if (options.collectorUrl.getProtocol().equals("http")) {
-        builder.usePlaintext();
-      }
-      return new GrpcCollectorClient(
-          tracer,
-          builder,
-          options.deadlineMillis
-      );
-    } catch (ManagedChannelProvider.ProviderNotFoundException e) {
-      // TODO - let the user know that they need to include a grpc channel dependency.
-      return null;
+    public static GrpcCollectorClientProvider provider() {
+        return INSTANCE;
     }
-  }
+
+    // public for reflective instantiation.
+    public GrpcCollectorClientProvider() {
+        ManagedChannelProvider.provider();
+    }
+
+    @Override
+    protected int priority() {
+        return 1;
+    }
+
+    @Override
+    GrpcCollectorClient forOptions(
+            AbstractTracer tracer,
+            Options options
+    ) {
+        try {
+            ManagedChannelBuilder<?> builder;
+            if (options.grpcCollectorTarget != null) {
+                builder = ManagedChannelBuilder.forTarget(options.grpcCollectorTarget);
+            } else {
+                builder = ManagedChannelBuilder.forAddress(
+                    options.collectorUrl.getHost(),
+                    options.collectorUrl.getPort()
+                );
+            }
+
+            if (options.grpcRoundRobin) {
+                builder.defaultLoadBalancingPolicy("round_robin");
+            }
+
+            if (options.collectorUrl.getProtocol().equals("http")) {
+                builder.usePlaintext();
+            }
+            return new GrpcCollectorClient(
+                    tracer,
+                    builder,
+                    options.deadlineMillis
+            );
+        } catch (ManagedChannelProvider.ProviderNotFoundException e) {
+            // TODO - let the user know that they need to include a grpc channel dependency.
+            return null;
+        }
+    }
 }
